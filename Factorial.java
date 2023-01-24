22a82b15441bc5ac3420a78456d681caefe20d72
VAIBHAVMANIK <81464192+VAIBHAVMANIK@users.noreply.github.com>
2023-01-24 22:19:06 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,14 @@
+public class Factorial{
+  public static void main(String [] args){
+      int first = 0;
+      int second =1;
+      int third = 1;
+      System.out.print(first+" "+second" ")
+      while(third<100){
+        System.out.print(" "+third);
+        first = second;
+        second = third;
+        third = first+second;
+      }
+  }
+}
